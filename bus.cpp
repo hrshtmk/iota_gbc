43ffc1a34d71f26e205577a9553ffd45732b4e56
hrshtmk <harshit007k@gmail.com>
2026-06-30 00:05:29 +0530

    Implemented Memory Banking.

--- bus.cpp
+++ bus.cpp
@@ -1,19 +1,59 @@
 #include "bus.hpp"
+#include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
-// Initializing flat 64KB memory with 0s.
-BUS::BUS(){ Memory.resize(65536, 0x00);}
+BUS::BUS(){
+    VRAMBankSelect = 0;
+    WRAMBankSelect = 1;
+    IERegister = 0x00;
+
+    //Zeroing buffers.
+    fill(&VRAMBanks[0][0], &VRAMBanks[0][0]+(2*8192), 0x00);
+    fill(&WRAMBanks[0][0], &WRAMBanks[0][0]+(8*4096), 0x00);
+    fill(OAM, OAM+160, 0x00);
+    fill(IORegisters, IORegisters+128, 0x00);
+    fill(HRAM, HRAM+127, 0x00);
+    
+    InitIOHandler();
+}
+
+void BUS::InitIOHandler(){
+    for (int i = 0; i < 128; i++) {
+        IOregisterTable[i] = nullptr;
+    }
+    IOregisterTable[0x04] = &BUS::DIVwrite;
+    IOregisterTable[0x4F] = &BUS::VBKwrite;
+    IOregisterTable[0x70] = &BUS::SVBKwrite;
+
+}
+
+void BUS::DIVwrite(uint8_t value) {
+    IORegisters[0x04] = 0x00; // Any write to DIV resets it to 0.
+}
+
+void BUS::VBKwrite(uint8_t value) {
+    VRAMBankSelect = value & 0x01; // GBC VRAM bank 0 or 1.
+    IORegisters[0x4F] = value;
+}
+
+void BUS::SVBKwrite(uint8_t value) {
+    uint8_t bank = value & 0x07;
+    if (bank == 0) bank = 1; //bank 0 acts as bank 1.
+    WRAMBankSelect = bank;
+    IORegisters[0x70] = value;
+}
 
 // Reading value at address.
-uint8_t BUS::read(uint16_t address) const {return Memory[address];}
+uint8_t BUS::read(uint16_t address) const {
+    //TODO with Banking.
+}
 
 // Writing value at address.
 void BUS::write(uint16_t address, uint8_t value){
-    if (address < 0x8000) return;
-    Memory[address] = value;
+    //TODO.
 }
 
 // File handling.
@@ -35,11 +75,5 @@ bool BUS::loadRom(const string& filename) {
     }
 
     cout << "\n[Loaded " << filename << " of " << size << " bytes]\n";
-    
-    size_t bytesToCopy = min(static_cast<size_t>(size), static_cast<size_t>(32768));
-    for (size_t i = 0; i < bytesToCopy; ++i) {
-        Memory[i] = CartridgeRom[i];
-    }
-
     return true;
 }
--- bus.hpp
+++ bus.hpp
@@ -12,6 +12,27 @@ class BUS {
         void write(uint16_t address, uint8_t value);
 
     private:
-        vector<uint8_t> Memory;
+        
         vector<uint8_t> CartridgeRom;
+        uint8_t VRAMBanks[2][8192]; //Video RAM: 2 Banks of 8KB.
+        uint8_t WRAMBanks[8][4096]; //Work RAM: 8 Banks of 4KB.
+        uint8_t OAM[160];           //sprites.
+        uint8_t IORegisters[128];   //Memory-mapped IO (0xFF00 - 0xFF7F).
+        uint8_t HRAM[127];          // High RAM (0xFF80 - 0xFFFE)
+        uint8_t IERegister;        // Interrupt Enable (0xFFFF)
+
+        // Memory Bank States.
+        uint8_t VRAMBankSelect;
+        uint8_t WRAMBankSelect;
+
+        // FPT for IO writes.
+        typedef void (BUS::*IOWriteHandler)(uint8_t value);
+        IOWriteHandler IOregisterTable[128];
+        
+        void InitIOHandler();
+        
+        void IOwrite(uint16_t address, uint8_t value);
+        void DIVwrite(uint8_t value);
+        void VBKwrite(uint8_t value);
+        void SVBKwrite(uint8_t value);
 };
